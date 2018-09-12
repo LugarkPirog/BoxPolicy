4f400f006593459446ea6f8729cb93c082b4d8c5
Lugark <sabaka4ka@mail.ru>
2018-09-12 17:17:45 +0300

    added features for stability. Now works time to time

--- model.py
+++ model.py
@@ -15,6 +15,8 @@ class PolicyAgent:
         self.q_val_target,\
             self.action_input,\
             self.loss,\
+            self.grads,\
+            self.grad_placeholders,\
             self.tr_step = self.create_updater()
 
         self.sess.run(tf.global_variables_initializer())
@@ -25,7 +27,7 @@ class PolicyAgent:
             with tf.variable_scope('layer1'):
                 w1 = tf.get_variable('w', (self.state_dim, layer_sizes[0]))
                 b1 = tf.Variable(tf.zeros((layer_sizes[0],)),name='b')
-                l1 = tf.nn.relu(tf.matmul(state_in, w1) + b1)
+                l1 = tf.nn.relu(tf.matmul(state_in / 150., w1) + b1)
             with tf.variable_scope('layer2'):
                 w2 = tf.get_variable('w', (layer_sizes[0], layer_sizes[1]))
                 b2 = tf.Variable(tf.zeros((layer_sizes[1],)), name='b')
@@ -33,21 +35,26 @@ class PolicyAgent:
             with tf.variable_scope('layer3'):
                 w3 = tf.get_variable('w', (layer_sizes[1], self.action_dim))
                 b3 = tf.Variable(tf.zeros((self.action_dim,)), name='b')
-                out = tf.nn.sigmoid(tf.matmul(l2, w3) + b3)
+                out = tf.nn.softmax(tf.matmul(l2, w3) + b3)
             picked = tf.argmax(out, axis=-1)
             vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.name)
         return state_in, out, picked, vars
 
     def create_updater(self):
-        q_target = tf.placeholder(tf.float32, (None, 1), name='q_target')
-        act_placeholder = tf.placeholder(tf.int32, (None, 1), name='action_plh')
+        q_target = tf.placeholder(tf.float32, (None), name='q_target')
+        act_placeholder = tf.placeholder(tf.int32, (None), name='action_plh')
         resp_inds = tf.range(0, tf.shape(self.action_output)[0])*self.action_dim + act_placeholder
         resp_outs = tf.gather(tf.reshape(self.action_output, [-1]), resp_inds)
 
-        loss = -tf.log(resp_outs)*q_target
+        loss = -tf.reduce_mean(tf.log(resp_outs)*q_target)
+        grads = tf.gradients(loss, self.net)
+        grad_plh = []
+        for var in self.net:
+            grad_plh.append(tf.placeholder(tf.float32, name=var.name[:-2]+'_holder'))
+
         up = tf.train.AdamOptimizer(1e-3)
-        tr_step = up.minimize(loss)
-        return q_target, act_placeholder, loss, tr_step
+        tr_step = up.apply_gradients(zip(grad_plh, self.net))
+        return q_target, act_placeholder, loss, grads, grad_plh, tr_step
 
     def get_actions(self, states):
         return self.sess.run(self.actions, feed_dict={self.state_input:states})
@@ -61,10 +68,14 @@ class PolicyAgent:
     def load(self):
         pass
 
-    def update(self, states, actions, rewards):
-        self.sess.run(self.tr_step, feed_dict={self.state_input:states,
+    def get_grads(self, states, actions, rewards):
+        return self.sess.run(self.grads, feed_dict={self.state_input:states,
                                                self.action_input:actions,
                                                self.q_val_target:rewards})
+
+    def update(self, grads):
+        self.sess.run(self.tr_step, feed_dict=dict(zip(self.grad_placeholders,grads)))
+
 
 def get_discounted_reward(arr, gamma=.98):
     ans = np.zeros_like(arr, dtype=np.float32)
@@ -74,50 +85,85 @@ def get_discounted_reward(arr, gamma=.98):
         ans[i] = moving_rew
     return ans
 
+
 def process_distances(arr):
-    last = 200
+    last = abs(arr[0])
     ans = []
     for i in range(1, len(arr)):
         ans.append(int(abs(arr[i]) < last))
         last = abs(arr[i])
-    return ans + ans[-1:]
+    return ans + ans[-1:] if abs(arr[-1]) > 1. else ans + [50]
 
+if __name__ == '__main__1':
+    import numpy as np
+    env = Env(max_steps=250)
+    iters = []
+    for _ in range(1000):
+        env.set_manual_game(150., 10.)
+        done = False
+        i = 0
+        while not done:
+            i += 1
+            st, dist, done = env.step(1)
+        iters.append(i)
+
+
+    print(np.mean(iters))
 
 if __name__ == '__main__':
     import numpy as np
-    env = Env()
+    env = Env(max_steps=150)
     agent = PolicyAgent(2, 3)
 
-    rounds = 5000
+    rounds = 30000
+    update_every = 30
     verbose = 100
-    eps = 0.2
-    eps_decay=.99
+    gs = 0
     total_rewards = []
+    total_len= []
+    #env.set_manual_game(140.,100.)
+    env.reset()
+
+    grad_buffer = np.array([tf.zeros_like(k).eval(session=agent.sess) for k in agent.net])
+    data = []
     for r in range(rounds):
+        gs += 1
         state = env.get_state()
         target = env.get_target()
         if abs(state - target) < 1:
+            #env.set_manual_game(140., 100.)
             env.reset()
             continue
         buffer = []
+        state_from, state_to = state, target
+        step = 0
         while True:
-            act = agent.get_actions(np.reshape([state, target], (-1, 2)))
-            if np.random.rand() < eps:
-                act = np.random.randint(0, 3)
+            step += 1
+            act = agent.get_action_distr(np.reshape([state, target], (-1, 2)))
+            act = np.random.choice([0, 1, 2], p=act[0])
             new_state, distance, done = env.step(act)
-            buffer.append([[state, target], act, distance])
+            buffer.append([[state, target], act, distance, done])
             state = new_state
             if done:
                 buffer = np.array(buffer)
                 states = np.vstack(buffer[:,0])
-                actions = np.asarray(buffer[:,1]).reshape((-1,1))
-                pd = process_distances(buffer[:, 2])
-                rewards = np.asarray(get_discounted_reward(pd)).reshape((-1,1))
+                actions = np.asarray(buffer[:,1])
+                pd = process_distances(buffer[:, 2], )
+                rewards = np.asarray(get_discounted_reward(pd))
 
-                agent.update(states, actions, rewards)
+                grads = agent.get_grads(states, actions, rewards)
+                for ix, gr in enumerate(grads):
+                    grad_buffer[ix] += gr
+                #env.set_manual_game(140., 100.)
                 env.reset()
+                total_rewards.append(np.mean(pd))
+                total_len.append(step)
                 break
-        total_rewards.append(np.mean(pd))
-        eps*=eps_decay
+        data.append((state_from, state_to, step))
+        if gs % update_every == update_every - 1:
+            agent.update(grad_buffer)
         if r % verbose == verbose - 1:
-            print(np.mean(total_rewards[-verbose:]))
+            print(r+1, np.mean(total_rewards[-verbose:]), np.mean(total_len[-verbose:]))
+
+    import pandas as pd
+    pd.DataFrame(data).to_csv('/home/user/Desktop/policy_results.csv')
